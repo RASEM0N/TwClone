b7ff7c89d59a2501c8ad3dfba50f098daaf25efd
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-04 11:49:50 +0500

    add: Main page всякие штучки

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,24 @@
 import {
+    Button,
     Container,
-    createStyles,
     Grid,
     makeStyles,
     Paper,
+    TextField,
     Typography,
-    InputBase,
-    withStyles,
 } from '@material-ui/core'
 
 import React from 'react'
+import InfoMenu from './InfoMenu'
 import SideMenu from './SideMenu'
 import TweetItem from './TweetItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import ListItem from '@material-ui/core/ListItem'
+import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined'
+import EmojiEmotionsOutlinedIcon from '@material-ui/icons/EmojiEmotionsOutlined'
+import DataUsageOutlinedIcon from '@material-ui/icons/DataUsageOutlined'
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -20,9 +27,11 @@ const useStyles = makeStyles((theme) => ({
 
     //#region Side menu
     sideMenu: {
+        top: 0,
+        position: 'sticky',
         height: '100vh',
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
     },
     sideMenuList: {
         listStyle: 'none',
@@ -57,6 +66,7 @@ const useStyles = makeStyles((theme) => ({
         borderRight: 0,
         borderRadius: 0,
         padding: '10px 15px',
+
         '& h6': {
             fontWeight: 700,
         },
@@ -65,15 +75,14 @@ const useStyles = makeStyles((theme) => ({
     //#region Messages
     contentMenuButtonGroup: {
         display: 'flex',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
 
     contentMenuIconButton: {
-        padding: '20px 0px 5px'
+        padding: '20px 0px 5px',
     },
     contentMenuIcon: {
         fontSize: 24,
-
     },
     //#endregion
 
@@ -81,16 +90,16 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 // кастомный элемент
-const SearchInput = withStyles(() =>
-    createStyles({
-        input: {
-            borderRadius: 30,
-            backgroundColor: '#E6ECF0',
-            height: 45,
-            padding: 0,
-        },
-    })
-)(InputBase)
+// const SearchInput = withStyles(() =>
+//     createStyles({
+//         input: {
+//             borderRadius: 30,
+//             backgroundColor: '#E6ECF0',
+//             height: 45,
+//             padding: 0,
+//         },
+//     })
+// )(InputBase)
 
 const Home = () => {
     const classes = useStyles()
@@ -100,28 +109,112 @@ const Home = () => {
             <Container maxWidth={'lg'} className={classes.container}>
                 <Grid container spacing={2}>
                     <Grid item xs={3} className={classes.sideMenu}>
-                        <SideMenu classes={classes}/>
+                        <SideMenu classes={classes} />
                     </Grid>
                     <Grid item xs={6}>
                         <Paper className={classes.contentMenu} variant={'outlined'}>
-                            <Paper className={classes.contentHeader} variant={'outlined'}>
+                            <Paper
+                                className={classes.contentHeader}
+                                variant={'outlined'}
+                                style={{
+                                    position: 'sticky',
+                                    top: '0',
+                                    zIndex: 1,
+                                    borderBottomWidth: 14,
+                                }}
+                            >
                                 <Typography variant="h6">Главная</Typography>
+                                <ListItem
+                                    style={{
+                                        marginTop: 15,
+                                    }}
+                                >
+                                    <ListItemAvatar
+                                        style={{
+                                            alignSelf: 'end',
+                                        }}
+                                    >
+                                        <Avatar style={{
+                                            width: 50,
+                                            height: 50
+                                        }} src='https://data.whicdn.com/images/300076584/original.jpg'/>
+                                    </ListItemAvatar>
+                                    <div
+                                        style={{
+                                            width: '100%',
+                                        }}
+                                    >
+                                        <TextField
+                                            id="outlined-multiline-static"
+                                            multiline
+                                            placeholder="Что происходит?"
+                                            variant="outlined"
+                                            style={{
+                                                width: '100%',
+                                            }}
+                                        />
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: 'space-between',
+                                                marginTop: 15
+
+                                            }}
+                                        >
+                                            <div>
+                                                <IconButton>
+                                                    <ImageOutlinedIcon color="primary" />
+                                                </IconButton>
+                                                <IconButton>
+                                                    <EmojiEmotionsOutlinedIcon color="primary" />
+                                                </IconButton>
+                                            </div>
+
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <div>269</div>
+                                                <IconButton>
+                                                    <DataUsageOutlinedIcon color="primary" />
+                                                </IconButton>
+                                                <Button variant={'contained'} color="primary">
+                                                    Твитнуть
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {/*<IconButton>*/}
+                                    {/*    <PersonAddIcon />*/}
+                                    {/*</IconButton>*/}
+                                </ListItem>
                             </Paper>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
-                            <TweetItem classes={classes}/>
+
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
+                            <TweetItem classes={classes} />
                         </Paper>
                     </Grid>
-                    <Grid item xs={3}>
-                        <SearchInput fullWidth placeholder={'Поиск по Твиттеру'} />
+                    <Grid
+                        item
+                        xs={3}
+                        style={{
+                            position: 'sticky',
+                            top: 0,
+                            height: '100vh',
+                        }}
+                    >
+                        <InfoMenu />
                     </Grid>
                 </Grid>
             </Container>
--- src/components/Home/SideMenu.tsx
+++ src/components/Home/SideMenu.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import { IconButton, Button, Typography } from '@material-ui/core'
+import {
+    IconButton,
+    Button,
+    Typography,
+    Hidden,
+    TextField,
+    List,
+    ListItemIcon,
+    Paper,
+} from '@material-ui/core'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import SearchIcon from '@material-ui/icons/Search'
 import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined'
@@ -7,19 +16,33 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline'
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 import ViewListOutlinedIcon from '@material-ui/icons/ViewListOutlined'
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import Collapse from '@material-ui/core/Collapse'
 
 type PropsType = {
     classes: any
 }
 
 const SideMenu: React.FC<PropsType> = ({ classes }) => {
+    const [open, setOpen] = React.useState(false)
+
+    const handleClick = () => {
+        setOpen(!open)
+    }
+
     return (
         <>
             <div
                 style={{
                     display: 'flex',
                     flexDirection: 'column',
-                    flex: 1
+                    flex: 1,
                 }}
             >
                 <ul className={classes.sideMenuList}>
@@ -32,37 +55,49 @@ const SideMenu: React.FC<PropsType> = ({ classes }) => {
                         <IconButton>
                             <SearchIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Поиск</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Поиск</Typography>
+                        </Hidden>
                     </li>
                     <li>
                         <IconButton>
                             <NotificationsNoneOutlinedIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Уведомление</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Уведомление</Typography>
+                        </Hidden>
                     </li>
                     <li>
                         <IconButton>
                             <MailOutlineIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Сообщение</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Сообщение</Typography>
+                        </Hidden>
                     </li>
                     <li>
                         <IconButton>
                             <BookmarkBorderIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Закладки</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Закладки</Typography>
+                        </Hidden>
                     </li>
                     <li>
                         <IconButton>
                             <ViewListOutlinedIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Список</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Список</Typography>
+                        </Hidden>
                     </li>
                     <li>
                         <IconButton>
                             <PersonOutlineIcon className={classes.sideMenuIcon} />
                         </IconButton>
-                        <Typography variant={'h6'}>Профиль</Typography>
+                        <Hidden smDown>
+                            <Typography variant={'h6'}>Профиль</Typography>
+                        </Hidden>
                     </li>
                 </ul>
                 <Button
@@ -79,10 +114,47 @@ const SideMenu: React.FC<PropsType> = ({ classes }) => {
                     Твитнуть
                 </Button>
             </div>
-            <div style={{
-                height: 100,
-
-            }}>User profile ....</div>
+            <Paper variant="outlined">
+                <List>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar src="https://data.whicdn.com/images/300076584/original.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText primary="Dock of Shame" secondary="@FavDockOfChannel" />
+                        <IconButton onClick={handleClick}>
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </IconButton>
+                    </ListItem>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            <ListItem
+                                button
+                                style={{
+                                    paddingLeft: 70,
+                                }}
+                            >
+                                <ListItemText primary="Starred" />
+                            </ListItem>
+                            <ListItem
+                                button
+                                style={{
+                                    paddingLeft: 70,
+                                }}
+                            >
+                                <ListItemText primary="Starred" />
+                            </ListItem>
+                            <ListItem
+                                button
+                                style={{
+                                    paddingLeft: 70,
+                                }}
+                            >
+                                <ListItemText primary="Starred" />
+                            </ListItem>
+                        </List>
+                    </Collapse>
+                </List>
+            </Paper>
         </>
     )
 }
